1892c894728ae09ec986ba65057bef203dba180f
Kumbhar19 <roshnikumbhar.k@gmail.com>
2024-09-26 18:57:37 +0530

    I have implemented Initial cards

--- Blackjack.py
+++ Blackjack.py
@@ -7,5 +7,14 @@ def create_deck():
         for suit in suits:
             deck.append(value + suit)
     return deck
-# deck = create_deck
-# print(create_deck())
+
+def deal_initial_cards(deck):
+    player_hand = []
+    dealer_hand = []
+    for i in range(2):
+        random_index = random.randint(0, len(deck) -1)
+        player_hand.append(deck.pop(random_index))
+        random_index = random.randint(0, len(deck) -1)
+        dealer_hand.append(deck.pop(random_index))
+    return player_hand, dealer_hand
+
