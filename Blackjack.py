14422fff6f4aadc9c37396c3278d43aa6b91ef48
Kumbhar19 <roshnikumbhar.k@gmail.com>
2024-09-26 19:25:23 +0530

    I have implemented Calculated hand

--- Blackjack.py
+++ Blackjack.py
@@ -18,3 +18,19 @@ def deal_initial_cards(deck):
         dealer_hand.append(deck.pop(random_index))
     return player_hand, dealer_hand
 
+def calculate_hand_value(hand):
+    values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'J': 10, 'Q': 10, 'K': 10, 'A': 1}
+    value = 0
+    for card in hand:
+        card_value = card[:-1]
+        value += values[card_value]
+    ace_in_hand = False
+    for card in hand:
+        if card[:-1] == 'A':
+            ace_in_hand = True 
+            break
+    if ace_in_hand and value + 10 <= 21:
+        value += 10
+    return value
+
+
